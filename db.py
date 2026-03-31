ff8697e0cb724a748166492eff3575794ac71f81
Shaun Whitt <blackteawhitt@gmail.com>
2026-03-31 12:09:07 -0400

    Fixing db for schema

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
-import psycopg
 import os
+import psycopg
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
@@ -7,3 +7,13 @@ def get_db():
     conn = psycopg.connect(DATABASE_URL)
     conn.autocommit = True
     return conn
+
+def init_db():
+    conn = get_db()
+    cur = conn.cursor()
+
+    with open("schema.sql", "r", encoding="utf-8") as f:
+        cur.execute(f.read())
+
+    cur.close()
+    conn.close()
